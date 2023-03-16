4a5654ff7166265c57ee066f1c675d1f8b4f786d
Chris Bray <zenandjuice@gmail.com>
2023-03-16 14:33:55 -0500

    Update user_enrollment_history.sql

--- enrollments/user_enrollment_history.sql
+++ enrollments/user_enrollment_history.sql
@@ -1,19 +1,19 @@
 -- this returns a specified user or course and shows the enrollment history
 -- based on a query from Corrie Bergeron
 
-select cm.course_id, 
-  u.user_id,
-  cu.role, 
+select cm.course_id, u.user_id, cu.role, 
     CASE 
         WHEN cu.ROW_STATUS = 0 THEN 'Enabled'
         WHEN cu.ROW_STATUS = 2 THEN 'Disabled'
         ELSE 'OTHER'
     END AS ROW_STATUS,
-  cu.available_ind as "Availability", 
+  cu.available_ind as "Availability",  
   dscu.batch_uid as "Enrollment DSK", 
   cu.enrollment_date as "Enrollment Date", 
   cu.dtmodified as "Date Enrollment Modified", 
-  cu.last_access_date as "Last Access Date", 
+  cu.last_access_date as "Last Access Date",  
+  cm.start_date as "Course Start Date",
+  cm.end_date  as "Course End Date",
   cu.bypass_course_avail_until "Extented Access Until",
   cm.dtcreated as "Date Course Created"
 FROM course_users cu
