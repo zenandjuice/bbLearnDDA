335e0d9227c80cb62f0ef21a2c6f49205f274ccb
Chris Bray <zenandjuice@gmail.com>
2023-07-14 09:06:02 -0500

    Update user_activity-older_data.sql

--- activity_and_auth/user_activity-older_data.sql
+++ activity_and_auth/user_activity-older_data.sql
@@ -1,6 +1,5 @@
--- Uses stats database to get Course activity older than the 180 days in bblearn.aa
-
--- alter session set current_schema = ..._stats
+-- Uses _stats schema to get Course activity older than the 180 days stored in the main schema activity_accumulator table
+-- alter schema to be _stats
 
 SELECT u.user_id AS Username, u.uuid as "User UUID", u.pk1 as "User PK1", cm.pk1 as "Course PK1", cm.course_id, aa.timestamp, aa.event_type, aa.internal_handle, aa.data, aa.session_id
 	FROM activity_accumulator aa
