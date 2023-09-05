351604aa7114c036f22b92e58a23443e02b17e22
Chris Bray <zenandjuice@gmail.com>
2023-09-05 18:45:13 -0500

    Update linkedin_learning.sql
    Added LTI 1.3 tools

--- integrations/linkedin_learning.sql
+++ integrations/linkedin_learning.sql
@@ -1,3 +1,28 @@
+-- LTI 1.3 Placements
+
+select distinct(cc.pk1) as "Content pk1",
+ cm.course_id, cm.pk1 as "Course PK1",
+ STRING_AGG(u.email, ';') AS emails,
+ string_agg(distinct u.firstname||' '||u.lastname, ', ') as instructors,
+ cc.title as "Link Title",
+ ccp.title as "Folder Title",
+ cc.cnthndlr_handle as "Tool",
+ cc.link_ref,
+ cc.web_url
+FROM course_main cm
+INNER JOIN course_contents cc ON cc.crsmain_pk1 = cm.pk1
+left join course_contents ccp on cc.parent_pk1 = ccp.pk1
+join course_users cu on cm.pk1 = cu.crsmain_pk1
+join users u on cu.users_pk1 = u.pk1
+where cc.cnthndlr_handle = 'resource/x-bb-blti-link'
+and cc.web_url like '%linked%'
+AND cu.role = 'P' and cu.row_status = '0'
+--and cm.course_id like "%[string]%"
+GROUP BY cc.pk1, cm.course_id, cm.pk1, cc.title, ccp.title
+order by cm.course_id
+
+
+
 -- LinkedIn Learning Tools
 --  https://bbooglecom.fatcow.com/Blackboard-Partners/lil_50x50.jpg
 
