74385fa4ac6a8e857542db231a285d3c41bc7ff0
Chris Bray <zenandjuice@gmail.com>
2024-04-16 13:14:28 -0500

    Update loginas_logs.sql
    Added institutional roles for impersonated user, and changed the variables for impersonator vs. impersonated

--- activity_and_auth/loginas_logs.sql
+++ activity_and_auth/loginas_logs.sql
@@ -1,15 +1,25 @@
--- Base query that returns impersonator and impersonated user
+-- Base query that returns impersonator and impersonated user 
 
-select si.created "LoginAs Date", u2.user_id as "Impersonated User", u.user_id as "Impersonated by", si.reason as "Reason", si.session_id 
+select si.created "LoginAs Date", u.user_id as "Impersonated User", ir.role_id as "Institutional Role", u2.user_id as "Impersonated by", si.reason as "Reason", si.session_id 
 from sessions_impersonated si
-inner join users u on si.impersonator_pk1 = u.pk1 
-inner join users u2 on si.impersonated_pk1 = u2.pk1
-order by si.created 
+	inner join users u on si.impersonated_pk1 = u.pk1
+	inner join users u2 on si.impersonator_pk1 = u2.pk1
+	inner join institution_roles ir on u.institution_roles_pk1 = ir.pk1
+order by si.created
 
+--count of which roles we've impersonated
+ 
+select ir.role_id, count(ir.role_id) 
+from sessions_impersonated si
+	inner join users u on si.impersonated_pk1 = u.pk1
+	inner join users u2 on si.impersonator_pk1 = u2.pk1
+	inner join institution_roles ir on u.institution_roles_pk1 = ir.pk1
+group by ir.role_id 
+order by ir.role_id
 
 --LoginAs Information with Activity Accumulator data
 
-select si.created "LoginAs Date", u2.user_id as "Impersonated User", u.user_id as "Impersonated by", si.reason as "Reason", si.session_id,
+select si.created "LoginAs Date", u.user_id as "Impersonated User", ir.role_id  as "Institutional Role", u2.user_id as "Impersonated by", si.reason as "Reason", si.session_id,
        cm.course_id,
        cc.title,
        aa.timestamp,
@@ -18,10 +28,12 @@ select si.created "LoginAs Date", u2.user_id as "Impersonated User", u.user_id a
        aa.content_pk1,
        aa.event_type
 from sessions_impersonated si
-inner join users u on si.impersonator_pk1 = u.pk1 
-inner join users u2 on si.impersonated_pk1 = u2.pk1
-inner join activity_accumulator aa on si.session_id = aa.session_id 
-LEFT JOIN course_main cm ON aa.course_pk1 = cm.pk1
-left JOIN course_contents cc ON aa.content_pk1 = cc.pk1
-WHERE aa.timestamp between '02/01/2023 00:00:00' and '02/06/2023 23:59:59'
+	inner join users u on si.impersonated_pk1 = u.pk1
+	inner join users u2 on si.impersonator_pk1 = u2.pk1
+	inner join institution_roles ir on u.institution_roles_pk1 = ir.pk1
+	inner join activity_accumulator aa on si.session_id = aa.session_id 
+	LEFT JOIN course_main cm ON aa.course_pk1 = cm.pk1
+	left JOIN course_contents cc ON aa.content_pk1 = cc.pk1
+WHERE aa.timestamp between '03/01/2024 00:00:00' and '04/30/2024 23:59:59'
+--AND u.user_id = '[username]' -- check for specific user
 order by si.created 
